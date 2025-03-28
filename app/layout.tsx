1b54d4c1f224d62807ed8cf61190479730830968
Shashwat <goswamishashwatpuri1@gmail.com>
2025-03-28 23:09:12 +0530

    added dashboard layout and root layout, mobile, desktop sidebar and clerk config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, DM_Sans } from "next/font/google";
 import "./globals.css";
 
+import AppProviders from "@/components/providers/app-providers";
+import { Toaster } from "sonner";
+import { ClerkProvider } from "@clerk/nextjs";
+
 const inter = Inter({ subsets: ["latin"] });
+const dmSans = DM_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +20,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <ClerkProvider
+    afterSignOutUrl={'/sign-in'}
+    appearance={{
+      elements: {
+        formButtonPrimary: 'bg-primary hover:bg-primary/90 text-sm !shadow-none',
+      },
+    }}
+  >
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <AppProviders>{children}</AppProviders>
+        <Toaster richColors />
+      </body>
     </html>
+  </ClerkProvider>
   );
 }
